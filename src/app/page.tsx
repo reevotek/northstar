1cf067380b5596fcd8a2604bf13e3f7eb69a1e09
reevotek <reevotek@outlook.com>
2024-11-09 11:38:04 -0800

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hola Mundo!</h1>
-      <div>
-        <Button>Sign in</Button>
-      </div>
+    <div className="bg-slate-900 ">
+      <main className="flex flex-col justify-center items-center mx-auto text-center max-w-5xl h-dvh">
+        <div className="bg-slate-700 flex flex-col gap-6 p-12 rounded-xl text-white w-4/5 sm:max-w-96 sm:text-2xl">
+          <h1 className="text-5xl mb-6">REI Shop</h1>
+          <address>
+            1790 Expo Pkwy <br />
+            Sacramento, CA 95815
+          </address>
+          <p>Open daily: 9am to 6pm</p>
+          <Link href="tel:916 924 8900" className="hover:underline">
+            916 924 8900
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
